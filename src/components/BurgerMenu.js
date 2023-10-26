1f2d2001b47d5d78be8f603880f2df635781afb3
Felipe Gomes <contato@felipe-gomes.com>
2023-10-26 19:50:06 -0300

    Remove Link from BurgerMenu

--- src/components/BurgerMenu.js
+++ src/components/BurgerMenu.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { slide as Menu } from 'react-burger-menu';
-import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 
 export default function BurgerMenu() {
